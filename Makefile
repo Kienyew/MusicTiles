73effedb978c46547c01b349d1c453178b6a1287
Kienyew <aobi100bt@hotmail.com>
2020-08-09 21:50:15 +0800

    Move list implementation from main.c into seperate file

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = gcc
 CFLAGS = -g -Wall -I.
 LDFLAGS = -lpthread -lm -lX11 -ldl
 RAYLIB_HEADERS = raylib.h
-OBJECTS = sound.o
+OBJECTS = sound.o list.o
 
 
 main: main.c $(RAYLIB_HEADERS) $(OBJECTS)
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <math.h>    /* fabsf(), cos(), sin() */
 #include <limits.h>
 
+#include "list.h"
 #include "sound.h"
 
 #ifdef _WIN32
@@ -63,19 +64,6 @@
 // ------ END MACROS ------
 
 // ------ STRUCTURES ------
-typedef struct List
-{
-	struct ListNode* head;
-	struct ListNode* tail;
-	size_t size;
-} List;
-
-typedef struct ListNode
-{
-	struct ListNode* prev;
-	struct ListNode* next;
-	void* data;
-} ListNode;
 
 
 typedef struct MusicNote
@@ -175,75 +163,6 @@ float rect_intersect_area(Rectangle a, Rectangle b)
 		return fabsf((MAX(a.x, b.x) - MIN(a.x + a.width, b.x + b.width)) * (MAX(a.y, b.y) - MIN(a.y + a.height, b.y + b.height)));
 }
 
-// data will be copied.
-static void list_append_tail(List * list, const void* data, size_t data_size)
-{
-	if (list->size == 0)
-	{
-		assert(list->head == NULL && list->tail == NULL);
-		list->head = list->tail = calloc(1, sizeof(ListNode));
-		list->tail->data = memcpy(malloc(data_size), data, data_size);
-	}
-	else
-	{
-		assert(list->tail != NULL && list->tail->prev == NULL);
-		list->tail->prev = calloc(1, sizeof(ListNode));
-		list->tail->prev->data = memcpy(malloc(data_size), data, data_size);
-		list->tail->prev->next = list->tail;
-		list->tail = list->tail->prev;
-	}
-
-	list->size++;
-}
-
-// User is responsible to free the returned pointer by free()
-static void* list_pop_head(List * list)
-{
-	assert(list->head != NULL);
-	assert(list->head->next == NULL);
-
-	void* ret = list->head->data;
-	ListNode * old_head = list->head;
-	if (list->size == 1)
-	{
-		list->head = list->tail = NULL;
-	}
-	else
-	{
-		list->head = old_head->prev;
-		list->head->next = NULL;
-	}
-	free(old_head);
-	list->size--;
-	return ret;
-}
-
-// list_pop_tail should be declared in general, but I didn't used that in overall, so I omitted it to reduce code size.
-// static void* list_pop_tail(List* list);
-
-// User is responsible to free the returned pointer
-static void* list_pop_node(List * list, ListNode * target)
-{
-	assert(list != NULL && target != NULL);
-	assert(list->size != 0 && "What do you want to pop from an empty list?");
-
-	if (target->prev) target->prev->next = target->next;
-	if (target->next) target->next->prev = target->prev;
-	if (target == list->head) list->head = target->prev;
-	if (target == list->tail) list->tail = target->next;
-
-	list->size--;
-	void* ret = target->data;
-	free(target);
-	return ret;
-}
-
-static void list_clear(List * list, void(*free_function)(void*))
-{
-	if (list == NULL) return;
-	while (list->size > 0)
-		free_function(list_pop_head(list));
-}
 
 // data will be copied
 // RETURN: true if registration successful else false
